937c423783c89cc6a390996b1dbe6fe177f6ac47
bonthajohn <s10241552@connect.np.edu.sg>
2024-06-30 22:15:19 +0800

    asg2

---
+++ Scripts/LevelRestart.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestart : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+}
